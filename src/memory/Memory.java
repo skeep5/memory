40e4a84f8ffcaf6172746102c61118c6ffacf635
Brandon <Brandon@Uwey>
2014-01-24 18:03:48 -0700

    Update .gitignore
    Updated from the L02 preparation folder in class.

--- src/memory/Memory.java
+++ src/memory/Memory.java
@@ -6,27 +6,44 @@
 
 package memory;
 
+import java.util.Scanner;
+
 /**
  *
- * @author Beej
+ * @author Brandon^2
  */
 public class Memory {
-
     String name;
-    String instructions = "This is the Memory Game! \n\n";
-    
+    String instructions = "This game is a memory game!\n\n"
+            + "You will be chosing cards from a field that will have various\n"
+            + "values on the back of the card. In order to score a point\n"
+            + "you must create a match by turning over two cards of the same\n"
+            + "value. The cards that are paired will then be removed from the\n"
+            + "game and your turn will continue. Mispair your selection and \n"
+            + "your opponent will get a turn. The player with the most pairs\n"
+            + "will win the game!";
+            
     /**
      * @param args the command line arguments
      */
-    
-   function displayName {
-    System.out.println("name \n");
-}
-      function instructions {
-    System.out.println("instructions \n");
-}
     public static void main(String[] args) {
+        Memory myGame = new Memory();
+        myGame.getName();
+        myGame.displayHelp();
+        
         // TODO code application logic here
+     
+        
+    }
+    public void getName(){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your nickname: ");
+        this.name = input.next();
+        
+    }
+    public void displayHelp(){
+        System.out.println("\nWelcome " + this.name + ",\n");
+        System.out.println(this.instructions);
     }
     
 }
