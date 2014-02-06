1724873a6e7da24b0330ef3d2510b8410dff9b54
Brandon <Brandon@Uwey>
2014-02-06 16:04:15 -0700

    Merge origin/master
    Conflicts:
    	src/memory/Board.java

--- src/memory/HelpMenuView.java
+++ src/memory/HelpMenuView.java
@@ -1,15 +1,90 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
+ * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package memory;
+
+import java.util.Scanner;
+
+
 
 /**
  *
- * @author Brandon Larsen & Brandon Urednick
+ * @author BrandonU
  */
-public class HelpMenuView {
+public class HelpMenuView  {
+        
+    private final static String[][] menuItems = {
+        {"B", "The board"},
+        {"C", "A computer player"}, 
+        {"G", "The Tic-Tac-Toe game"},
+        {"L", "A location"},
+        {"M", "A marker"},
+        {"R", "A regular player"},        
+        {"Q", "Quit Help"}        
+    };
     
+    // Create instance of the HelpMenuControl (action) class
+    private HelpMenuControl helpMenuControl = new HelpMenuControl();
+    
+    // default constructor
+    public HelpMenuView() {
+        
+    } 
+    
+    // display the help menu and get the end users input selection
+    public void getInput() {       
+              
+        String command;
+        Scanner inFile = new Scanner(System.in);
+        
+        do {
+            
+            this.display(); // display the menu
+            
+            // get commaned entered
+            command = inFile.nextLine();
+            command = command.trim().toUpperCase();
+            
+            switch (command) {
+                case "B":
+                    this.helpMenuControl.displayBoardHelp();
+                    break;
+                case "C":
+                    this.helpMenuControl.displayComputerPlayerHelp();
+                    break;
+                case "G":
+                    this.helpMenuControl.displayGameHelp();
+                    break;                  
+                case "L":
+                    this.helpMenuControl.displayLocationHelp();
+                    break;
+                case "M":
+                    this.helpMenuControl.displayMarkerHelp();
+                    break;
+                 case "R":
+                    this.helpMenuControl.displayRealPlayerHelp();
+                    break; 
+                case "Q": 
+                    break;
+                default: 
+                    new memoryError().displayError("Invalid command. Please enter a valid command.");
+                    continue;
+            }
+        } while (!command.equals("Q"));  
+        
+         return;
+    }
+
+        // displays the help menu
+    public final void display() {
+        System.out.println("\n\t===============================================================");
+        System.out.println("\tEnter the letter associated with one of the following commands:");
+
+        for (int i = 0; i < HelpMenuView.menuItems.length; i++) {
+            System.out.println("\t   " + menuItems[i][0] + "\t" + menuItems[i][1]);
+        }
+        System.out.println("\t===============================================================\n");
+    }
+  
 }
